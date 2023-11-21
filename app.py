ac5690922a2ce6fd4eb7dfc8b99cec44bb029ff6
Tim Sereda <46585644+asquishyfishy@users.noreply.github.com>
2023-11-20 18:40:39 -0600

    Saves audio

--- app.py
+++ app.py
@@ -1,16 +1,27 @@
-from flask import Flask, render_template
-import random
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
+
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/random-number')
-def random_number():
-    number = random.randint(1, 100)
-    return f'<p>Your random number is: {number}</p>'
+@app.route('/upload-audio', methods=['POST'])
+def upload_audio():
+    if 'audio_data' not in request.files:
+        return 'No audio data', 400
+    file = request.files['audio_data']
+    if file:
+        filename = secure_filename(file.filename)
+        save_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(save_path)
+        
+    return f'File saved at {save_path}'
 
 if __name__ == '__main__':
     app.run(debug=True)
